520c3875e9549649ad104df46c48d9d6990ab581
PONASBULVIUS\Tomas <tomas.kiniulis@unity3d.com>
2017-06-30 00:04:39 +0300

    Adding Unity project
    first commit

---
+++ VideoTest/Assets/MODULES/module-video/Scripts/SliderControl.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;// Required when using Event data.
+using UnityEngine.Video;
+
+public class SliderControl : EventTrigger,  ISelectHandler// required interface when using the OnSelect method.
+{
+    VideoPlayer
+        m_Video;
+    AudioSource
+        m_AudioSource;
+    Slider
+        m_slider;
+    float
+       m_flengthVideo;
+
+    enum STATE_SLIDER
+    {
+        WAITING,
+        NOT_SELECTED,
+        SELECTED
+    }
+
+    STATE_SLIDER
+        m_ecurrentState = STATE_SLIDER.WAITING;
+
+    public void Awake()
+    {
+        m_slider = GetComponent<Slider>();
+    }
+
+    public void Initialize(VideoPlayer l_videoPlayer, AudioSource l_audioSource)
+    {
+        m_Video = l_videoPlayer;
+        m_AudioSource = l_audioSource;
+
+        m_flengthVideo = m_Video.frameCount / m_Video.frameRate;
+        m_ecurrentState = STATE_SLIDER.NOT_SELECTED;
+    }
+
+    //Do this when the selectable UI object is selected.
+    public override void OnPointerDown(PointerEventData data)
+    {
+        m_ecurrentState = STATE_SLIDER.SELECTED;
+    }
+
+    public override void OnPointerUp(PointerEventData data)
+    {
+        m_ecurrentState = STATE_SLIDER.NOT_SELECTED;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (m_ecurrentState)
+        {
+            case STATE_SLIDER.NOT_SELECTED:
+                {
+                    m_slider.value = (float)(m_Video.time / m_flengthVideo);
+                }
+                break;
+            case STATE_SLIDER.SELECTED:
+                {
+                    m_Video.time = m_slider.value * m_flengthVideo;
+                }
+                break;
+        }
+    }
+}
---
+++ VideoTest/Assets/MODULES/module-video/Scripts/StreamVideo.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using System;
+
+
+
+
+public class StreamVideo : MonoBehaviour
+{
+    public static event Action OnStreamVideoEndReached;
+
+    public RawImage image;
+    public GameObject playIcon;
+
+    public string
+        m_sUrl;
+
+    public VideoClip videoToPlay;
+
+    private VideoPlayer videoPlayer;
+
+    private AudioSource audioSource;
+
+    private bool firstRun = true;
+
+    void Awake()
+    {
+        image.uvRect = new Rect(0, 0, 1, 1);
+        image.CrossFadeAlpha(0.0f, 0.0f, false);
+    }
+
+    void Start()
+    {
+        LoadVideo();
+    }
+
+    void InitializeVideo()
+    {
+        firstRun = false;
+        //Add VideoPlayer to the GameObject
+        videoPlayer = gameObject.AddComponent<VideoPlayer>();
+
+        //Add AudioSource
+        audioSource = gameObject.AddComponent<AudioSource>();
+
+        //Disable Play on Awake for both Video and Audio
+        videoPlayer.playOnAwake = false;
+        audioSource.playOnAwake = false;
+        audioSource.Pause();
+
+        // Vide clip from Url
+        videoPlayer.source = VideoSource.Url;
+        videoPlayer.url = "https://s3.eu-central-1.amazonaws.com/admented/Alliance/2017-05/videos/agora.mp4";
+        videoPlayer.isLooping = false;
+
+        //Set Audio Output to AudioSource
+        videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+
+        //Assign the Audio from Video to AudioSource to be played
+        videoPlayer.EnableAudioTrack(0, true);
+        videoPlayer.SetTargetAudioSource(0, audioSource);
+
+        // :: EVENT
+        videoPlayer.prepareCompleted += Prepared;
+        videoPlayer.loopPointReached += EndReached;
+    }
+
+    void PlayVideo()
+    {
+        videoPlayer.Play();
+        audioSource.Play();
+        playIcon.SetActive(false);
+    }
+
+    void PauseVideo()
+    {
+        videoPlayer.Pause();
+        audioSource.Pause();
+        playIcon.SetActive(true);
+    }
+
+    void LoadVideo()
+    {
+        InitializeVideo();
+        //Set video To Play then prepare Audio to prevent Buffering
+        videoPlayer.Prepare();
+
+        //Wait until video is prepared
+    }
+
+    void EndReached(VideoPlayer l_vplayer)
+    {
+        Debug.Log("End Reached ");
+        if ( !videoPlayer.isLooping )
+            image.CrossFadeAlpha(0.0f, 0.5f, false);
+    }
+
+    void Prepared(UnityEngine.Video.VideoPlayer vPlayer)
+    {
+        //Assign the Texture from Video to RawImage to be displayed
+        SliderControl l_sliderControl = GetComponentInChildren<SliderControl>();
+        if (l_sliderControl != null )
+            l_sliderControl.Initialize(videoPlayer, audioSource);
+
+        image.texture = videoPlayer.texture;
+        image.CrossFadeAlpha(1.0f, 1.5f, false);
+
+        PlayVideo();
+    }
+}
